51768fd12dafd139c2a6f3c957102091cfe96b07
Nitin Chauhan <nitin.c.chauhan@ericsson.com>
2020-07-26 01:10:43 +0530

    Added Model

--- statreports/admin.py
+++ statreports/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import ReportRow
 
-# Register your models here.
+admin.site.register(ReportRow)
--- statreports/views.py
+++ statreports/views.py
@@ -1,18 +1,27 @@
 from django.http import HttpResponse
 from .models import ReportRow
 from django.shortcuts import render, redirect
+import re
 
 
 class Entry(object):
-    def __init__(self, NAME, SIZE, MAXSIZE):
+    def __init__(self, NAME, ADDRESS, ACTIVE, INACTIVE, MAX_ACTIVE, COUNT, ERRORS, TIMEOUTS, LATENCY, PEAK_LATENCY, THROUGHPUT):
         super(Entry, self).__init__()
         self.NAME = NAME
-        self.SIZE = SIZE
-        self.MAXSIZE = MAXSIZE
+        self.ADDRESS = ADDRESS
+        self.ACTIVE = ACTIVE
+        self.INACTIVE = INACTIVE
+        self.MAX_ACTIVE = MAX_ACTIVE
+        self.COUNT = COUNT
+        self.ERRORS = ERRORS
+        self.TIMEOUTS = TIMEOUTS
+        self.LATENCY = LATENCY
+        self.PEAK_LATENCY = PEAK_LATENCY
+        self.THROUGHPUT = THROUGHPUT
         self.entries = []
 
     def __str__(self):
-        return self.NAME + " " + self.SIZE
+        return self.NAME + " " + self.ADDRESS + " " + self.COUNT + " " + self.ERRORS
 
     def __repr__(self):
         return self.__str__()
@@ -31,21 +40,22 @@ def stats(request):
         dest.close()
 
     entries = []
-    file = open('./cluster.txt', 'r').readlines()
+    file = open('./client.txt', 'r').readlines()
     title = file.pop(0)
     timeStamp = file.pop(0)
     header = file.pop(0)
 
     for line in file:
-        words = line.split()
-        NAME, SIZE, MAXSIZE = [i for i in words]
-        entry = Entry(NAME, SIZE, MAXSIZE)
+        words = re.split(r'  +', line.lstrip())
+        NAME, ADDRESS, ACTIVE, INACTIVE, MAX_ACTIVE, COUNT, ERRORS, TIMEOUTS, LATENCY, PEAK_LATENCY, THROUGHPUT = [
+            i for i in words]
+        entry = Entry(NAME, ADDRESS, ACTIVE, INACTIVE, MAX_ACTIVE,
+                      COUNT, ERRORS, TIMEOUTS, LATENCY, PEAK_LATENCY, THROUGHPUT)
         entries.append(entry)
 
-        maxEntry = max(entries, key=lambda entry: entry.NAME)
-        print(maxEntry)
+    maxEntry = max(entries, key=lambda entry: entry.COUNT)
+    print(maxEntry)
 
-    #rows = ReportRow.objects.order_by('name')
-    #context = {'rows': rows}
-    # return render(request, 'statreports/index.html', context)
-    return HttpResponse('Stats')
+    rows = ReportRow.objects.order_by('name')
+    context = {'rows': rows}
+    return render(request, 'statreports/index.html', context)
