895766e40907826d8927008bd177a23f270b80b8
Nitin Chauhan <nitin.c.chauhan@ericsson.com>
2020-08-15 14:37:31 +0530

    Localhost check on client parent tab

--- statreports/admin.py
+++ statreports/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ClientRow, ServerRow, AlarmRow, CharsRow
+from .models import ClientRow, ServerRow, AlarmRow, CharsRow, ClientParentRow
 
 admin.site.register(ClientRow)
 admin.site.register(ServerRow)
 admin.site.register(AlarmRow)
 admin.site.register(CharsRow)
+admin.site.register(ClientParentRow)
