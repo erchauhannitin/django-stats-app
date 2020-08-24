2cbfc2bf22126f75c18c6ae05c9ee750d1985b05
Nitin Chauhan <nitin.c.chauhan@ericsson.com>
2020-08-24 16:34:16 +0530

    MInor label changes

--- statreports/views/views_clientparent_summary.py
+++ statreports/views/views_clientparent_summary.py
@@ -17,15 +17,40 @@ def clientparent_json(request):
 
     chart_type = request.session.get('chart_type')
     dataset = ClientParentRow.objects.order_by('-count')[0:5]
+    dataset2 = ClientParentRow.objects.order_by('-errors')[0:5]
+    dataset3 = ClientParentRow.objects.order_by('-latency')[0:5]
 
     chart = {
-        'chart': {'type': chart_type},
+        'chart': {'type': chart_type, 'options3d': {
+            'enabled': 'true',
+            'alpha': '45',
+            'beta': '0',
+            'depth': '50',
+            'viewDistance': '25'
+        }},
+        'plotOptions': {
+            'pie': {
+                'innerSize': '100',
+                'depth': '45',
+            }
+        },
         'title': {'text': 'Problematic clients'},
         'series': [{
-            'name': 'Client Name',
+            'name': 'Client count',
             'colorByPoint': 'true',
             'data': list(map(lambda row: {'name': row.name, 'y': row.count}, dataset))
-        }]
+        },
+            {
+            'name': 'Client errors',
+            'colorByPoint': 'true',
+            'data': list(map(lambda row: {'name': row.name, 'y': row.errors}, dataset2))
+        },
+            #     {
+            #     'name': 'Client latency',
+            #     'colorByPoint': 'true',
+            #     'data': list(map(lambda row: {'name': row.name, 'y': int(row.latency)}, dataset3))
+            # }
+        ]
     }
 
     return JsonResponse(chart)
