0127720f99157f65ea15465c8a327ad74e30a9ca
Nitin Chauhan <nitin.c.chauhan@ericsson.com>
2020-09-14 14:05:07 +0530

    Renaming proj folder

--- django_project/settings.py
+++ django_project/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'django_project.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
 # Database
